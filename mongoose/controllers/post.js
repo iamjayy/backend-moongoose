4faa994794ba9b5e368ae663f81e0d47c38a9bc6
jason <jason_doan94@yahoo.com>
2019-08-19 04:23:35 -0400

    delete

--- mongoose/controllers/post.js
+++ mongoose/controllers/post.js
@@ -1,21 +1,22 @@
-const Post = require('../models/post');
-const formidable = require('formidable');
-const fs = require('fs');
+const Post = require("../models/post");
+const formidable = require("formidable");
+const fs = require("fs");
 
 exports.getPosts = (req, res) => {
-  const posts = Post.find().select("_id title body")
-        .then(posts => {
-      res.json({ posts })
-  })
-  .catch(err => console.log(err));
+  const posts = Post.find()
+    .populate("postedBy", "_id name")
+    .select("_id title body")
+    .then(posts => {
+      res.json({ posts });
+    })
+    .catch(err => console.log(err));
 };
 
-
 exports.createPost = (req, res, next) => {
-  let form = new formidable.IncomingForm()
-  form.keepExtensions = true
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
-    if(err){
+    if (err) {
       return res.status(400).json({
         error: "image could not be uploaded"
       });
@@ -24,20 +25,73 @@ exports.createPost = (req, res, next) => {
 
     req.profile.hased_password = undefined;
     req.profile.salt = undefined;
-   // console.log("PROFILE", req.profile)
+    // console.log("PROFILE", req.profile)
 
     post.postedBy = req.profile;
-    if(files.photo){
-      post.photo.data = fs.readFileSync(files.photo.path)
-      post.photo.contentType = files.photo.type
+    if (files.photo) {
+      post.photo.data = fs.readFileSync(files.photo.path);
+      post.photo.contentType = files.photo.type;
     }
     post.save((err, result) => {
-      if(err) {
+      if (err) {
         return res.status(400).json({
           error: err
         });
       }
       res.json(result);
+    });
+  });
+};
+
+exports.postsByUser = (req, res) => {
+  Post.find({ postedBy: req.profile._id })
+    .populate("postedBy", "_id name")
+    .sort("_created")
+    .exec((err, posts) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      res.json(posts);
+    });
+};
+
+exports.postById = (req, res, next, id) => {
+  Post.findById(id)
+    .populate("postedBy", "_id name")
+    .exec((err, post) => {
+      if (err || !post) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      req.post = post;
+      next();
+    });
+};
+
+exports.isPoster = (req, res, next) => {
+  let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+  
+  if (!isPoster) {
+    return res.status(403).json({
+      error: "user not authorized"
+    });
+  }
+  next();
+};
+
+exports.deletePost = (req, res) => {
+  let post = req.post;
+  post.remove((err, post) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.json({
+      message: "post deleted successfully"
     });
   });
 };
--- mongoose/routes/post.js
+++ mongoose/routes/post.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const { getPosts, createPost } = require('../controllers/post');
-const { requireSignin } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
-const {createPostValidator } = require('../validators')
+const express = require("express");
+const {
+  getPosts,
+  createPost,
+  postsByUser,
+  postById,
+  isPoster,
+  deletePost
+} = require("../controllers/post");
+const { requireSignin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
+const { createPostValidator } = require("../validators");
 
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get("/", getPosts);
 router.post(
-    '/post/new/:userId', 
-    requireSignin,  
-    createPost,
-    createPostValidator
-    );
+  "/post/new/:userId",
+  requireSignin,
+  createPost,
+  createPostValidator
+);
+
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost)
 
 router.param("userId", userById);
+router.param("userId", postById);
 
 module.exports = router;
-
